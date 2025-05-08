300d73cc187d82455e18829885db34f6e1bde340
gerarb1 <gerardoburgos1232@gmail.com>
2025-05-08 14:52:08 -0400

    segunda practica

---
+++ mientras.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    inicio <BR>
+    <?php
+    $i=0;
+    while ($i<10)
+    {
+        echo " el valor de i es","<br>";
+        $i++;
+    }
+    fin <BR>
+</body>
+</html>
